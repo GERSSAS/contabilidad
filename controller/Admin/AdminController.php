ab98af922d846cd72e4ff161e60268bed9de147c
nicollearcos <nicolle.arcos@gers.com>
2023-11-24 09:50:43 -0500

    se agrega el proyecto

---
+++ controller/Admin/AdminController.php
@@ -0,0 +1,2 @@
+<?php include_once './model/Admin/AdminModel.php';
+?>
---
+++ lib/conf/conf.php
@@ -0,0 +1,7 @@
+<?php
+    $host="localhost";
+    $user="root";
+    $pass="";
+    $database="gers_contable1";
+    $port="3306"; 
+?>
